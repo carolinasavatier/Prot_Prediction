6b5fc6de00bed619f7514478fdfbb0f81ac2070f
carolinasavatier <savatiercarolina@gmail.com>
2017-02-23 18:32:14 +0100

    Add files via upload

--- Bioinformatics_projects/bin/parsingscript.py
+++ Bioinformatics_projects/bin/parsingscript.py
@@ -1,6 +1,5 @@
 #Create one big list with the whole raw data file (each line is an element of the list, so we have title1, sequence1, structure1, title2...). Then create a list with all the titles,titlelist, another with all the sequences, seqlist, and another with all the structures, structurelist)
 
-#import pandas as pd
 f = open("/home/u2195/Desktop/Bioinformatics_projects/data/test.txt", "r")
 datalist = list()
 for line in f:
@@ -21,18 +20,18 @@ for i in datalist[2::3]:
 #Create a dictionary containing the amino acids as keys and vectors defining each aa as values.
 
 aa_dict = { 'A':[1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'C':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-			'D':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'E':[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'F':[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'G':[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'H':[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0], 
-			'I':[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], 
-			'K':[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0], 
-			'L':[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0], 
-			'M':[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0], 
-			'N':[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
- 			'Q':[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0],
+			'C':[0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+			'D':[0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'E':[0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'F':[0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'G':[0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'H':[0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'I':[0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0], 
+			'K':[0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0], 
+			'L':[0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0], 
+			'M':[0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0], 
+			'N':[0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0],
+ 			'Q':[0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0],
 			'P':[0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0], 
 			'R':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0], 
 			'S':[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0], 
@@ -49,38 +48,63 @@ structure_dict = { 'G':0, 'S':1 }
 
 window = 3
 
-#Now we need to create words with the specified windows size
+#Now we need to create words with the specified windows size. Windows -1 is because 
 wordlist = list()
+countwordlist = 0
 for i in range (0, len (seqlist)):
 	for j in range(0, (len(seqlist[i])-(window-1))):
 		wordlist.append(seqlist[i][j:j+(window)])
+		countwordlist += 1
 print(wordlist)
 
 #Create a big vector for each word and store it in a list (bigvectorlist)
 bigvectorlist=list()
+countvectorlist = 0
 for i in wordlist:
 	vectorlist= list()
 	for j in i:
 		vectorlist.extend(aa_dict[j])
+		countvectorlist += 1
 	bigvectorlist.append(vectorlist)
 print (bigvectorlist)	
- 
-#Create a list for the structure corresponding to each word
+
+#Create a list for the structure corresponding to each word (wordstructlist):
 import math
-wstructlist=list()
-alist= list()
+wordstructlist= list()
+countwordstructlist = 0 
 for i in range (0, len (structurelist)):
 	for j in range(0, (len(structurelist[i])-(window-1))):
-		wstructlist.append(structurelist[i][(math.floor(window/2)):(structurelist[i]-(window-1))])
-	
-print (wstructlist)
+		a = list(structurelist[i][j:j+(window)])
+		for feature in a:
+			del a[0]
+			del a[-1]		
+		countwordstructlist += 1
+		wordstructlist.append(a)
 
+#List flatening (because I had a list in a list):
 
+wordstructlist = [j for i in wordstructlist for j in i]
+print (wordstructlist)
+
+#Create a vector (structvectorlist) for each structure-word:
+structvectorlist=list()
+countstructvectorlist = 0 
+for i in wordstructlist:
+	vectorlist2 = list()
+	vectorlist2.append(structure_dict[i])
+	structvectorlist.append(vectorlist2)
+	countstructvectorlist+= 1
+print (structvectorlist)
+
+print (countwordlist, countvectorlist, countwordstructlist, countstructvectorlist)
 
 #Store it in a numpy array (because it's the input format).
 
+import numpy as np
+X = np.array(bigvectorlist)
+Y = np.array(structvectorlist)
+#print (X, Y)
+print (np.shape(Y))
 
 
 
-#For monday: X = np.array([[-1, -1], [-2, -1], [1, 1], [2, 1]])
-#y = np.array([1, 1, 2, 2])
